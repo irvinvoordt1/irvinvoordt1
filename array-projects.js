4ec4437490922c88a3b7f394424cc69e9b00b17c
Irvin van Oordt <35196855+irvinvoordt1@users.noreply.github.com>
2018-03-06 17:33:39 -0500

    array-project.js

---
+++ array-projects.js
@@ -0,0 +1,13 @@
+var score = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54];
+var solution1 = score [0];
+var solution2 = score [1];
+var solution3 = score [2];
+var solution4 = score [3];
+alert("Solution 3 produced " + solution3 + " bubbles!");
+
+var flavors = ["cannabis", "Chunky Monkey", " chocolate", " French Vanilla", "Rum Raisin" ];
+flavors[0]= "Vanilla ChocoChip";
+
+var products = ["hadlebars", "mobile phones", "pens", "pencils" ];
+var lastProductmade = products.length-1;
+var recentproductmade= products[lastProductmade];
