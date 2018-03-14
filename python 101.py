94394273d2350fd9424fb36e6fcb4f62910f7251
Irvin van Oordt <irvinvoordt1@gmail.com>
2018-03-14 13:38:45 -0400

    Update python 101.py

--- python 101.py
+++ python 101.py
@@ -39,3 +39,11 @@ print('The name of the ice-cream is "Sweet\'n\'Tasty"')
 
 q = len(dont_worry)
 print(q)
+
+#The % operator after a string is used to combine a string with variables. The % operator will replace %s in a string
+#with the string variable that comes after it. The %d special symbol is used as a placeholder for numeric or decimal values. 
+
+name = "John"
+print("Hello, PyCharm! My name is %s!" % name)     # Note: %s is inside the string, % is after the string
+
+print("I'm %d years old" % 28)
